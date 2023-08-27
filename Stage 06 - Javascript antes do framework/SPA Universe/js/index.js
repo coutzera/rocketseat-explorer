2be82792bdf99873aa97b9c0b3d2813223ca0c5e
coutzera <coutzera@outlook.com.br>
2023-08-27 11:16:28 -0300

    Corrigindo o arquivo de rotas

--- Stage 06 - Javascript antes do framework/SPA Universe/js/index.js
+++ Stage 06 - Javascript antes do framework/SPA Universe/js/index.js
@@ -1,10 +1,10 @@
 import { Router } from './router.js'
 
 const router = new Router();
-router.add("/", "http://127.0.0.1:5500/Stage%2006%20-%20Javascript%20antes%20do%20framework/pages/home.html");
-router.add("/universe", "http://127.0.0.1:5500/Stage%2006%20-%20Javascript%20antes%20do%20framework/pages/universe.html");
-router.add("/exploration", "http://127.0.0.1:5500/Stage%2006%20-%20Javascript%20antes%20do%20framework/pages/exploration.html");
-router.add(404, "http://127.0.0.1:5500/Stage%2006%20-%20Javascript%20antes%20do%20framework/pages/404.html");
+router.add("/", "./pages/home.html");
+router.add("/universe", "./pages/universe.html");
+router.add("/exploration", "./pages/exploration.html");
+router.add(404, "./pages/404.html");
 
 // Lida com o redirecionamento quando a página é carregada
 router.handle();
